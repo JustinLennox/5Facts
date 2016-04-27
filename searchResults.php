5d32c3be7401cb1dc2c68e9a9ba7a1a9d09816d7
nst16 <nealsthacker@gmail.com>
2016-04-27 01:29:44 -0400

    search algorithm started

--- searchResults.php
+++ searchResults.php
@@ -44,20 +44,50 @@
 			$variable->execute();
 
 			//output results table
-			echo "<table> <tr> <td><b>Events related to search: </b>". 
-				htmlspecialchars($_POST["search"]) . "</td></tr>";
+			//echo "<table> <tr> <td><b>Events related to search: </b>". 
+				//htmlspecialchars($_POST["search"]) . "</td></tr>";
 				
+			//create array for search
+			$searchResult = array();
 			$count = 1;
 			while($table = $variable->fetch( PDO::FETCH_ASSOC )){ 
 				$eventID = $table['name'];
-				echo "<tr><td>" . $table['name'] . "<form id='eventForm' action ='eventDetails.php' method='post'><input type='hidden' name='eventID' value='$eventID'/><input type='submit' value='Select'/></form></td></tr>";
+
+				//check if search matches the name of an event
+				//then list those first
+				//create list of words of search
+				$searchWords = explode(" ", $_POST["search"]);
+				for($i = 0; $i < count($searchWords); $i++)
+				{
+					if(strpos($table['name'], ucwords($searchWords[$i])) !== false)
+					{
+						//check if it is already in the search results array
+						if(!in_array($table['name'], $searchResult))
+						{
+							$searchResult[count($searchResult) - 1] = $table['name'];
+						}
+						//echo "<tr><td>" . $table['name'];
+					}
+				}
+				//echo "<tr><td>" . $table['name'] . "<form id='eventForm' action ='eventDetails.php' method='post'><input type='hidden' name='eventID' value='$eventID'/><input type='hidden' name='votes' value=''/><input type='submit' value='Select'/></form></td></tr>";
 				
  				$count++;
 			}	
 			echo "</table>";
-			if($count == 1)
+
+			$count = 1;
+			echo "<table> <tr> <td><b>Events related to search: </b>". 
+				htmlspecialchars($_POST["search"]) . "</td></tr>";
+			foreach($searchResult as $x => $x_value){
+				echo "<tr><td>" . $x_value . "<form id='eventForm' action ='eventDetails.php' method='post'><input type='hidden' name='eventID' value='$eventID'/><input type='hidden' name='votes' value=''/><input type='submit' value='Select'/></form></td></tr>";
+				$count++;
+			}
+			echo "</table>";
+
+			//if there are no results
+			if(empty($searchResult))
 			{
-				echo "No Results";	
+				echo "No Results<br><br>";	
 			}
 			
 		
